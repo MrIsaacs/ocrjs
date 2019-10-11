2c2d6a0955224a6e1de9edf4530dd9fc265ee1ed
Ivan Ilic <me@mrisaacs.org>
2019-10-12 01:29:01 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="vue-app">
+    <TestPictureInput />
+  </div>
+</template>
+
+<script>
+import TestPictureInput from "./components/PictureInput.vue";
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+    components: {
+        TestPictureInput,
+    }
+})
+export default class VueApp extends Vue {
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
