ea5df544df37d7e72867cea2fd6351427554619b
Ivan Ilic <me@mrisaacs.org>
2019-11-18 16:38:43 +0100

    feat(Modal): add component modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-modal
+    id="modal-add"
+    title="Add your Image here"
+    size="xl">
+    <slot />
+  </b-modal>
+</template>
+
+<script>
+export default {
+    name: 'Modal',
+    data: function() {
+        return {
+            infoMessage: 'Crop your image in this window'
+        };
+    }
+};
+</script>
+
+<style scoped>
+</style>
