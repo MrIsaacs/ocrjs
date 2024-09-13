ed189765962d379afdebb8065660545b7b63c2c6
Ivan Ilic <me@mrisaacs.org>
2024-09-13 20:52:53 +0200

    chore: update base URL configuration
    - Changed the base URL to reflect the new environment
    - Ensure routing and API calls are adjusted to the updated base URL

--- dist/_nuxt/Cq0zMWgl.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./DlAUqK2U.js";import{o as r,c as t,aZ as s}from"./DM7DrkLp.js";const c={};function n(e,a){return r(),t("th",null,[s(e.$slots,"default")])}const _=o(c,[["render",n]]);export{_ as default};
